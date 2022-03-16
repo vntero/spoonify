21670e2df7949ae3a47113aba681042a168cccc4
vntero <shin.antero@gmail.com>
2022-03-16 15:43:34 +0000

    api info being fetched as expected

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react";
+
 function Popular() {
 
+useEffect(() => {
+    getPopular()
+},[])
+
 const getPopular = async () => {
-    const api = await fetch (`https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=10`)
-    const data = await api.json();
+    const api = await fetch (
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+    )
+    const data = await api.json()
     console.log(data)
 }
 
