0299c114fb5d90f220e312a05e6e676c6cd5318b
vntero <shin.antero@gmail.com>
2022-03-17 17:34:24 +0000

    carousel finished, added google font

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -12,11 +12,19 @@ useEffect(() => {
 },[])
 
 const getPopular = async () => {
-    const api = await fetch (
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
-    )
-    const data = await api.json()
-    setPopular(data.recipes)
+    const check = localStorage.getItem('popular')
+
+    if(check){
+        setPopular(JSON.parse(check))
+    }else{
+        const api = await fetch (
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+        )
+        const data = await api.json()
+        localStorage.setItem('popular', JSON.stringify(data.recipes))
+        setPopular(data.recipes)
+    }
+    
 }
 
   return (
@@ -33,7 +41,7 @@ const getPopular = async () => {
                     }}>
                     {popular.map((recipe) => {
                         return(
-                            <SplideSlide>
+                            <SplideSlide key={recipe.id}>
                                 <Card>
                                     <p>{recipe.title}</p>
                                     <img src={recipe.image} alt={recipe.title} />
