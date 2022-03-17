5daa2ec5fa5ed1c21589ee170627ac217bfbe8a2
vntero <shin.antero@gmail.com>
2022-03-17 17:11:18 +0000

    added gradient to enhance title on top of images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -37,6 +37,7 @@ const getPopular = async () => {
                                 <Card>
                                     <p>{recipe.title}</p>
                                     <img src={recipe.image} alt={recipe.title} />
+                                    <Gradient/>
                                 </Card>
                             </SplideSlide>
                         )
@@ -55,10 +56,41 @@ const Card = styled.div`
 min-height: 25rem;
 border-radius: 2rem;
 overflow: hidden;
+position: relative;
 
 img {
     border-radius: 2rem;
+    position: absolute;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 }
+
+p{
+    position: absolute;
+    z-index: 10;
+    left: 50%;
+    bottom: 0%;
+    transform: translate(-50%, 0%);
+    color: white;
+    width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+`
+
+const Gradient = styled.div`
+    z-index: 3;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
 `
 
 
